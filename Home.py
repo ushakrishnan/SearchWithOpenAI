6b0eccdca2d47304f163722408e2b3065bd6843b
ushakrishnan <ushapriya.krishnan@gmail.com>
2023-05-25 17:20:50 -0500

    fix charmap decode error

--- Home.py
+++ Home.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from pathlib import Path
 
-def read_markdown_file(md_file):
-    return Path(md_file).read_text(encoding='utf-8', errors='ignore')
+def read_markdown_file(markdown_file):
+    return Path(markdown_file).read_text(encoding='utf-8', errors='ignore')
 
 intro_markdown = read_markdown_file("README.md")
 st.markdown(intro_markdown, unsafe_allow_html=True)
