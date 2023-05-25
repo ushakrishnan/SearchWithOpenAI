2e02a4902138075e1ed070bba7f3dd7fc80747a8
ushakrishnan <ushapriya.krishnan@gmail.com>
2023-05-25 17:14:38 -0500

    Fix - charmap codec error

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from pathlib import Path
 
 def read_markdown_file(markdown_file):
-    return Path(markdown_file).read_text()
+    return Path(markdown_file).read_text(encoding='utf-8', errors='ignore')
 
 intro_markdown = read_markdown_file("README.md")
 st.markdown(intro_markdown, unsafe_allow_html=True)
